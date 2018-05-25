3441c7a093568570e985fa4553eabc5ead7b52b8
wangyang <yang.wang@zenjoy.net>
2018-05-26 00:49:09 +0800

    增加富文本编辑方式

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,14 @@ from django.db import models
 
 import django.utils.timezone as timezone
 
+from ckeditor.fields import RichTextField
+
 import os
 
 
 class news(models.Model):
     tittle = models.CharField(u'标题',max_length=250)
-    contact = models.TextField()
+    #contact = models.TextField()
+    contact = RichTextField(blank=True,null=True,verbose_name="内容")
     picture = models.ImageField(u'图片')
     time = models.DateTimeField(u'发表时间', default = timezone.now)
--- jcgame/settings.py
+++ jcgame/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'cars',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -123,9 +124,11 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = '/static/assets/pic/'
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(BASE_DIR,'static')
+
+CKEDITOR_JQUERY_URL ='/static/assets/js/jquery.min.js'
 
 
 MEDIA_URL='/upload/'
